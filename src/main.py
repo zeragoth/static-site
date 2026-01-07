6a542a739bb913e02b889fc48f2db6de13c36de1
Zeragoth <connerdredge@yahoo.com>
2026-01-07 14:59:43 -0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/main.py
    	modified:   src/test_markdown_blocks.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import os
 import shutil
+
 from copy_static import copy_files_recursive
+
 
 public_path = "./public"
 static_path = "./static"
 
 
-
 def main():
-    print(f"copying files from {static_path} to {public_path}...")
     if os.path.exists(static_path) and os.path.exists(public_path):
+        print(f"Deleting contents of {public_path}...")
         shutil.rmtree(public_path)
 
+        print("Copying static files...")
         copy_files_recursive(static_path, public_path)
-        print("files copied successfully!")
+        print("Files copied successfully!")
 
 
 main()
